7fc43da64d85e0b4d9d5bca5ea17bff11369415f
y9828 <y9828@naver.com>
2026-01-30 15:04:33 +0900

    [추가] 회원가입 성공 및 테스트성공

--- springMyBatisMember/src/main/java/com/zeus/controller/MemberController.java
+++ springMyBatisMember/src/main/java/com/zeus/controller/MemberController.java
@@ -1,20 +1,138 @@
 package com.zeus.controller;
 
+import java.util.List;
+
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.zeus.domain.Member;
+import com.zeus.service.MemberService;
 
 import lombok.extern.slf4j.Slf4j;
 
 @Controller
-@MapperScan
+@MapperScan(basePackages= "com.zeus.mapper")
 @Slf4j
 @RequestMapping("/member")
 public class MemberController {
 	@Autowired
-	private Member memberService;
+	private MemberService memberService; // 타입(MemberService)을 인터페이스나 서비스 클래스로 지정하세요.
+	
+	@GetMapping("/insertForm")
+	public String boardInsertForm(Model model) {
+		return "member/insertForm";
+	}
+	
+	@PostMapping("/insert")
+	public String boardInsert(Member member, Model model) {
+		log.info("insert board = " + member.toString());
+		try {
+			memberService.register(member);
+		} catch (Exception e) {
+			e.printStackTrace();
+			return "member/failed";
+		}
+		return "member/success";
+	}
+	
+	@GetMapping("/boardList")
+	public String boardList(Model model) {
+		log.info("boardList");
+		
+		try {
+			List<Member> boardList = memberService.list();
+			
+			model.addAttribute("boardList",boardList);
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		return "member/boardList";
+	}
+	
+	/*@GetMapping("/detail")
+	public String boardDetail(Board b, Model model) {
+		log.info("boardDetail board = "+b.toString());
+		
+		try {
+			Board board = boardService.read(b);
+			if(board == null) {
+				return "board/failed";
+			}
+			model.addAttribute("board",board);
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return "board/detail";
+	}
+	
+	@GetMapping("/delete")
+	public String boardDelete(Board board, Model model) {
+		log.info("boardDetail board = "+board.toString());
+		
+		try {
+			boardService.delete(board);
+		} catch (Exception e) {
+			e.printStackTrace();
+			model.addAttribute("message", "%d 님의 정보 삭제가 실패하였습니다.".formatted(board.getNo()));
+			return "board/failed";
+		}
+		model.addAttribute("message", "%d 님의 정보가 삭제되었습니다.".formatted(board.getNo()));
+		return "board/success";
+	}
+	
+	@GetMapping("/updateForm")
+	public String boardUpdateForm(Board b, Model model) {
+		log.info("updateForm board = "+b.toString());
+		
+		try {
+			Board board = boardService.read(b);
+			if(board == null) {
+				model.addAttribute("message", "%d 님의 정보가 없습니다".formatted(b.getNo()));
+				return "board/failed";
+			}
+			model.addAttribute("board", board);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return "board/updateForm";
+	}
+	
+	@PostMapping("/update")
+	public String updateBoard(Model model, Board board) {
+		log.info("updateBoard board = "+board.toString());
+		
+		try {
+			boardService.update(board);
+		} catch (Exception e) {
+			e.printStackTrace();
+			model.addAttribute("message", "%d 님의 게시판이 수정되지 않았습니다.".formatted(board.getNo()));
+			return "board/failed";
+		}
+		model.addAttribute("message", "%d 님의 게시판이 수정되었습니다.".formatted(board.getNo()));
+		return "board/success";
+	}
+	
+	@GetMapping("/search")
+	public String boardSearch(Model model, Board board) {
+		log.info("searchType = " + board.toString());
+		
+		try {
+			List<Board> boardList = boardService.boardSearch(board);
+			
+			model.addAttribute("boardList",boardList);
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return "board/boardList";
+	}*/
 
 }
--- springMyBatisMember/src/main/java/com/zeus/service/MemberServiceimpl.java
+++ springMyBatisMember/src/main/java/com/zeus/service/MemberServiceimpl.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.zeus.domain.Member;
+import com.zeus.domain.MemberAuth;
 import com.zeus.mapper.MemberMapper;
 
 @Service
@@ -17,8 +18,15 @@ public class MemberServiceimpl implements MemberService {
 	@Override
 	@Transactional
 	public int register(Member member) throws Exception {
-		// TODO Auto-generated method stub
-		return 0;
+		int count = mapper.create(member);
+		if(count > 0) {
+			MemberAuth memberAuth = new MemberAuth();
+			memberAuth.setNo(member.getNo());
+			memberAuth.setAuth("ROLE_USER");
+			mapper.createAuth(memberAuth);
+		}
+					
+		return count;
 	}
 
 	@Override
